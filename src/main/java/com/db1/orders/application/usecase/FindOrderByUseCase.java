57e73352a2c58d0eb3ccbc34fa5cfa16da69e4c5
jacklaine <jacklainecavalheiro1@gmail.com>
2026-03-24 18:28:08 -0300

    Endpoint consulta pedido

---
+++ src/main/java/com/db1/orders/application/usecase/FindOrderByUseCase.java
@@ -0,0 +1,22 @@
+package com.db1.orders.application.usecase;
+
+import org.springframework.stereotype.Service;
+
+import com.db1.orders.domain.interfaces.IOrderRepository;
+import com.db1.orders.domain.modal.Orders;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FindOrderByUseCase {
+
+    private final IOrderRepository orderRepository;
+
+    public Orders execute(String orderId) {
+        return orderRepository.findByOrderId(orderId);
+    }
+
+}
--- src/main/java/com/db1/orders/entrypoint/controller/OrderController.java
+++ src/main/java/com/db1/orders/entrypoint/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.db1.orders.entrypoint.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.db1.orders.application.dto.CreateOrderResponse;
 import com.db1.orders.application.usecase.CreateOrderUseCase;
+import com.db1.orders.application.usecase.FindOrderByUseCase;
 import com.db1.orders.domain.modal.Orders;
 
 import lombok.RequiredArgsConstructor;
@@ -18,21 +21,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OrderController {
 
-    private final CreateOrderUseCase useCase;
+        private final CreateOrderUseCase createOrderUseCase;
+        private final FindOrderByUseCase findOrderByUseCase;
 
-    @PostMapping
-    public ResponseEntity<CreateOrderResponse> create(@RequestHeader("Idempotency-Key") String idempotencyKey,
-            @RequestBody Orders request) {
-        var createdOrder = useCase.execute(request, idempotencyKey);
-        var saved = new CreateOrderResponse(createdOrder.getId(), createdOrder.getOrderId(),
-                createdOrder.getStatus().getKey());
+        @PostMapping
+        public ResponseEntity<CreateOrderResponse> create(@RequestHeader("Idempotency-Key") String idempotencyKey,
+                        @RequestBody Orders request) {
+                var createdOrder = createOrderUseCase.execute(request, idempotencyKey);
 
-        CreateOrderResponse response = new CreateOrderResponse(
-                saved.getId(),
-                saved.getOrderId(),
-                saved.getStatus());
+                CreateOrderResponse response = new CreateOrderResponse(
+                                createdOrder.getId(),
+                                createdOrder.getOrderId(),
+                                createdOrder.getStatus().getKey());
 
-        return ResponseEntity.ok(response);
-    }
+                return ResponseEntity.ok(response);
+        }
+
+        @GetMapping("/{orderId}")
+        public ResponseEntity<Orders> getById(@PathVariable String orderId) {
+                Orders order = findOrderByUseCase.execute(orderId);
+
+                if (order == null) {
+                        return ResponseEntity.notFound().build();
+                }
+
+                return ResponseEntity.ok(order);
+        }
 
 }
